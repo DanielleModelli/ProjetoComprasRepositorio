d7189704c20e0f76e9c1fe5de61d2035fcaa1731
diegojosebarbosa <39098436+diegojosebarbosa@users.noreply.github.com>
2018-05-09 18:01:23 -0300

    adicionada Fornecedor_ProdutoRepository.java

--- Compras/src/main/java/br/edu/up/jpa/repository/Fornecedor_ProdutoRepository.java
+++ Compras/src/main/java/br/edu/up/jpa/repository/Fornecedor_ProdutoRepository.java
@@ -1,5 +1,7 @@
 package br.edu.up.jpa.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.edu.up.jpa.dominio.Fornecedor_Produto;
 
 
-//public interface Fornecedor_ProdutoRepository extends CrudRepository<Fornecedor_Produto Integer> {
-//}
+public interface Fornecedor_ProdutoRepository extends CrudRepository<Fornecedor_Produto Integer> {
+}
