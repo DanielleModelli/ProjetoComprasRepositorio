12c0826ec6e03cc8696dcd993c262eb82ac583d3
diegojosebarbosa <39098436+diegojosebarbosa@users.noreply.github.com>
2018-05-09 18:06:13 -0300

    classe produto .java

--- Compras/src/main/java/br/up/edu/jpa/dominio/Produto.java
+++ Compras/src/main/java/br/up/edu/jpa/dominio/Produto.java
@@ -1,2 +1,63 @@
 package br.up.edu.jpa.dominio;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "produto ")
+public class Produto {
+    @Id
+    @GeneratedValue
+    private Integer produtoId;
+
+    @Column
+    private String Nome;
+
+    @Column
+    private Integer Numero;
+
+    @Embedded
+    private Fornecedor_produto Fornecedor;
+
+    @OneToOne
+    private List<Fornecedor_produto> Fornecedor = new ArrayList<>();
+
+    public Produto (Fornecedor_produto Fornecedor, String Nome, Integer Numero) {
+        this.Fornecedor_produto = Fornecedor;
+        this.Nome  = Nome ;
+        this.Numero = Numero;
+        Fornecedor_produto = new ArrayList<>();
+    }
+
+    protected Produto() {
+    }
+
+    public Integer getProdutoId() {
+        return ProdutoId;
+    }
+
+    public nome getNome() {
+        return Nome;
+    }
+    public numero getNumero() {
+    	return Numero;
+    }
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+    public void setNumero(Integer Numero) {
+        this.Numero = Numero;
+    }
+
+    public List<Fornecedor_produto> getFornecedor() {
+        return Fornecedor  ;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto {" + "ProdutoId=" + ProdutoId + ", " + "nome =" + Nome + ", Num_Produto=" + Numero +
+                "}\n";
+    }
+}
 
