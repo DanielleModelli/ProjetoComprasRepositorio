a540a62002ca19283eceb6d687f67572a723b260
Andre Xavier <lordlagon@gmail.com>
2018-06-28 19:57:10 -0300

    Alteração unidadeMedida e Produto

--- Compras/src/main/java/br/edu/up/jpa/Resource/ProdutoResource.java
+++ Compras/src/main/java/br/edu/up/jpa/Resource/ProdutoResource.java
@@ -1,12 +1,7 @@
 package br.edu.up.jpa.Resource;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
